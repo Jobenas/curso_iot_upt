9dd1f623d8f0b0077dc281f82217f6abc5106309
Jorge <jobenas@gmail.com>
2024-12-03 22:05:57 -0500

    Agrega ejemplos de Python. Actualiza .gitignore

---
+++ sw_python/hola_mundo.py
@@ -0,0 +1,13 @@
+
+
+if __name__ == "__main__":
+    print("Hola mundo")
+
+    a = 2
+
+    if a == 2:
+        print("a es igual a 2")
+
+    a = "Esto es un nuevo string"
+
+    print(a)
---
+++ sw_python/promedios.py
@@ -0,0 +1,29 @@
+
+
+if __name__ == "__main__":
+    a = input("Ingrese el valor de a: ")
+    print(f"El resultado obtenido es {a}")
+
+    b = input("Ingrese el valor de b: ")
+    print(f"El resultado obtenido es {b}")
+
+    c = input("Ingrese el valor de c: ")
+    print(f"El resultado obtenido es {c}")
+
+    d = input("Ingrese el valor de d: ")
+    print(f"El resultado obtenido es {d}")
+
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+
+    p = a + b + c + d
+
+    print(f"El valor de p despues de la sumatoria es: {p}")
+    
+    p = p / 4
+
+    num_var = 4
+
+    print(f"El promedio obtenido para {num_var} variables es: {p}")
